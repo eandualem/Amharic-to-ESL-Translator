4a936379ece5a4872df381e4196d7018d2de2dfc
zelalemgetahun9374 <37741083+zelalemgetahun9374@users.noreply.github.com>
2020-12-04 09:07:22 -0800

    classes updated and added

---
+++ Assets/Scripts/LeftArmController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftArmController : MonoBehaviour
+{
+    // private Lerp lerp;
+    // private Slerp slerp;
+
+    // public bool moveArm(Vector3 start, Vector3 target, string transition, float waitTime)
+    // {
+    //     StartCoroutine(moveToTarget(start, target, transition, waitTime));
+    //     return true;
+    // }
+
+    // public IEnumerator moveToTarget(Vector3 start, Vector3 target, string transition, float waitTime)
+    // {
+    //     // Optimal values for speed
+    //     float lerpSpeed = 1.5f / waitTime;
+    //     float slerpSpeed = waitTime / 2f;
+
+    //     // Update pos for right arm
+    //     Vector3 startPos = new Vector3(start.x + 0.2f, start.y, start.z);
+    //     Vector3 targetPos = new Vector3(target.x + 0.2f, target.y, target.z);
+
+    //     slerp = GameObject.Find("LeftArmTarget").GetComponent<Slerp>();
+    //     lerp = GameObject.Find("LeftArmTarget").GetComponent<Lerp>();
+
+    //     LinearMovement(startPos, lerpSpeed);
+
+    //     yield return new WaitForSeconds(waitTime / 2);
+
+    //     switch (transition)
+    //     {
+    //         case "A": LinearMovement(targetPos, lerpSpeed); break;
+    //         case "B": CurvedMovementUp(targetPos, slerpSpeed); break;
+    //         case "C": CurvedMovementDown(targetPos, slerpSpeed); break;
+    //         case "D": CurvedMovementLeft(targetPos, slerpSpeed); break;
+    //         case "E": CurvedMovementRight(targetPos, slerpSpeed); break;
+    //         case "F": ZigzagMovement(targetPos, slerpSpeed); break;
+    //         case "G": CircularMovementHorizontal(targetPos, slerpSpeed); break;
+    //         case "H": CircularMovementVertical(targetPos, slerpSpeed); break;
+
+    //     }
+    // }
+
+    // private void LinearMovement(Vector3 targetPos, float speed)
+    // {
+    //     lerp.lerper(transform, targetPos, speed);
+    // }
+    // private void CurvedMovementUp(Vector3 targetPos, float speed)
+    // {
+    //     slerp.slerp(transform, targetPos, Vector3.up, speed);
+    // }
+    // private void CurvedMovementDown(Vector3 targetPos, float speed)
+    // {
+    //     slerp.slerp(transform, targetPos, Vector3.down, speed);
+    // }
+    // private void CurvedMovementLeft(Vector3 targetPos, float speed)
+    // {
+    //     slerp.slerp(transform, targetPos, Vector3.left, speed);
+    // }
+    // private void CurvedMovementRight(Vector3 targetPos, float speed)
+    // {
+    //     slerp.slerp(transform, targetPos, Vector3.right, speed);
+    // }
+    // private void ZigzagMovement(Vector3 targetPos, float speed)
+    // {
+    //     StartCoroutine(slerp.Zigzag(transform, targetPos, speed));
+    // }
+    // private void CircularMovementHorizontal(Vector3 targetPos, float speed)
+    // {
+    //     StartCoroutine(slerp.Circle(transform, targetPos, "H", speed));
+    // }
+    // private void CircularMovementVertical(Vector3 targetPos, float speed)
+    // {
+    //     StartCoroutine(slerp.Circle(transform, targetPos, "V", speed));
+    // }
+}
--- Assets/Scripts/LeftHandController.cs
+++ Assets/Scripts/LeftHandController.cs
@@ -1,127 +1,154 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LeftHandController : MonoBehaviour
 {
-   // left hand finger joins
-    public Transform thumb;
-    public Transform index;
-    public Transform middle;
-    public Transform ring;
-    public Transform pinky;
+     // right hand finger joins
+    // public Transform thumb;
+    // public Transform index;
+    // public Transform middle;
+    // public Transform ring;
+    // public Transform pinky;
 
-    public enum finger_conf_X : int { Straight, OneBent, TwoBent, TwoSlitleyBent, AllSlitleyBent, AllBent };
-    public enum finger_conf_Y : int { Open, Closed };
-    Transform[] fingers = new Transform[5];
+    // private float ang = 90.0f;
 
-    private Rotate rotate;
+    // Transform[] fingers = new Transform[5];
+    // Vector3[] startRot = new Vector3[15];
+    // HandConfig handConfig = new HandConfig();
 
-    private float speed = 1.0f;
+    // float[] xAngle;
+    // float[] zAngle;
 
-    private void move_finger(finger_conf_X[] conf_x, finger_conf_Y[] conf_y)
-    {
-        fingers[0] = thumb;
-        fingers[1] = index;
-        fingers[2] = middle;
-        fingers[3] = ring;
-        fingers[4] = pinky;
-
-        float[] xAngle = new float[3];
-        float[] zAngle = new float[3];
-
-        for (int f = 0; f < 5; f++)
-        {
-            switch (conf_x[f])
-            {
-                case finger_conf_X.Straight:        xAngle = new float[] {  0,  0,  0 }; break;
-                case finger_conf_X.OneBent:         xAngle = new float[] { 90,  0,  0 }; break;
-                case finger_conf_X.TwoSlitleyBent:  xAngle = new float[] {  0, 45, 45 }; break;
-                case finger_conf_X.TwoBent:         xAngle = new float[] {  0, 90, 90 }; break;
-                case finger_conf_X.AllSlitleyBent:  xAngle = new float[] { 45, 45, 45 }; break;
-                case finger_conf_X.AllBent:         xAngle = new float[] { 90, 90, 90 }; break;
-            }
-
-            switch (conf_y[f])
-            {
-                case finger_conf_Y.Open:    zAngle = new float[] { 15, 15, 15 }; break;
-                case finger_conf_Y.Closed:  zAngle = new float[] {  0,  0,  0 }; break;
-            }
-            Transform fingerJoint = fingers[f];
-            float[] angle = new float[3];
-            for (int i = 0; i < 3; i++)
-            {
-                angle[0] = xAngle[i];
-                angle[2] = zAngle[i];
-                rotate.RotateTarget(fingerJoint, angle, speed);
-                fingerJoint = fingerJoint.parent;
-            }
-        }
-    }
-
-    public bool moveFinger(string startConf, string endConf, float rotSpeed, float waitTime)
-    {
-        Debug.Log("------------------Conf----------------------------------");
-        Debug.Log("Start Conf: " + startConf + "|   End Conf: " + endConf);
-        Debug.Log("----------------------------------------------------");
-        rotate = GameObject.Find("RightArmTarget").GetComponent<Rotate>();
-
-        this.speed = rotSpeed;
-
-        configure(startConf.ToCharArray());
-        StartCoroutine(Wait(waitTime));
-        configure(endConf.ToCharArray());
-
-        return true;
-    }
+    // string startConf;
+    // string endConf;
 
 
-    IEnumerator Wait(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-    }
-    private void configure(char[] ca) 
-    {
-        finger_conf_X[] temp_x = { 
-                                    map_x(ca[0].ToString() + ca[1].ToString()),
-                                    map_x(ca[4].ToString() + ca[5].ToString()),
-                                    map_x(ca[8].ToString() + ca[9].ToString()),
-                                    map_x(ca[12].ToString() + ca[13].ToString()),
-                                    map_x(ca[16].ToString() + ca[17].ToString())
-                                   };
-        finger_conf_Y[] temp_y = {
-                                    map_y(ca[2].ToString() + ca[3].ToString()),
-                                    map_y(ca[6].ToString() + ca[7].ToString()),
-                                    map_y(ca[10].ToString() + ca[11].ToString()),
-                                    map_y(ca[14].ToString() + ca[15].ToString()),
-                                    map_y(ca[18].ToString() + ca[19].ToString())
-                                   };
-        move_finger(temp_x, temp_y);
-    }
-    //   F0X0Y0F1X0Y0F2X0Y0F3X0Y0F4X0Y0
-    finger_conf_X map_x(string s)
-    {
-        switch (s)
-        {
-            case "X0": return finger_conf_X.Straight;
-            case "X1": return finger_conf_X.OneBent;        
-            case "X2": return finger_conf_X.TwoSlitleyBent; 
-            case "X3": return finger_conf_X.TwoBent;        
-            case "X4": return finger_conf_X.AllSlitleyBent; 
-            case "X5": return finger_conf_X.AllBent;
+    // public bool moveFinger(string startConf, string endConf, float waitTime)
+    // {
+    //     Debug.Log("startConf: " + startConf);
+    //     Debug.Log("endConf: " + endConf);
 
-            default: return finger_conf_X.Straight;
-        }
-    }
+    //     this.startConf = startConf;
+    //     this.endConf = endConf;
 
-    finger_conf_Y map_y(string s)
-    {
-        switch (s)
-        {
-            case "Y0": return finger_conf_Y.Open;
-            case "Y1": return finger_conf_Y.Closed;
+    //     fingers[0] = thumb;
+    //     fingers[1] = index;
+    //     fingers[2] = middle;
+    //     fingers[3] = ring;
+    //     fingers[4] = pinky;
 
-            default: return finger_conf_Y.Open;
-        }
-    } 
+    //     this.CallWithDelay(0f,          InitialFingerConfiguration);
+    //     this.CallWithDelay(waitTime/2,  FinalFingerConfiguration);
+
+    //     return true;
+    // }
+
+    // void InitialFingerConfiguration()
+    // {
+    //     this.handConfig.configure(this.startConf.ToCharArray());
+    //     move_finger(this.handConfig.conf_x, this.handConfig.conf_y);
+
+    // }
+    // void FinalFingerConfiguration()
+    // {
+    //     this.handConfig.configure(this.endConf.ToCharArray());
+    //     move_finger(this.handConfig.conf_x, this.handConfig.conf_y);
+    // }
+
+    // private void move_finger(HandConfig.finger_conf_X[] conf_x, HandConfig.finger_conf_Y[] conf_y)
+    // {
+    //     // ---------------------------------Debug------------------------------------------------
+    //     string test = "";
+    //     foreach (var item in conf_x)
+    //     {
+    //         test += "| x : " + item;
+    //     }
+    //     Debug.Log(test);
+    //     // -------------------------------------------------------------------------------------
+
+    //     for (int f = 0; f < 5; f++)
+    //     {
+    //         xAngle = new float[] { 0, 0, 0 };
+    //         zAngle = new float[] { 0, 0, 0 };
+
+    //         if (f == 0) // thumb
+    //         {
+    //             switch (conf_x[f])
+    //             {
+    //                 // ---------------------------------------------- 0  ------------------ 1 ----------------- 2 ------------------ //
+    //                 case HandConfig.finger_conf_X.Straight:                                                                       break;
+    //                 case HandConfig.finger_conf_X.OneBent:                                                   xAngle[2] = ang/2;   break;
+    //                 case HandConfig.finger_conf_X.TwoSlitleyBent:  zAngle[0] = ang/3;  zAngle[1] = ang/4;  xAngle[2] = ang/2;   break;
+    //                 case HandConfig.finger_conf_X.TwoBent:         zAngle[0] = ang/2;  zAngle[1] = ang/3;  xAngle[2] = ang/2;   break;
+    //                 // Unique for thumb, BentOutt is AllSlitleyBent and 
+    //                 case HandConfig.finger_conf_X.AllSlitleyBent:  zAngle[0] = -ang/4;   zAngle[1] =  -ang/4;  xAngle[2] = ang/2;   
+    //                                                                                                          zAngle[2] = ang/6;  break;
+    //                 case HandConfig.finger_conf_X.AllBent:                                                   xAngle[2] = ang*2/3;
+    //                                                                                                          zAngle[2] = ang/6;  break;
+                    
+    //                 default: throw new Exception("Invalid Configuration");
+    //             }
+    //         }
+    //         else // The rest of fingers
+    //         {
+    //             switch (conf_x[f])
+    //             {
+    //                 // ---------------------------------------------- 0  ------------------ 1 ------------------- 2 ------------------ //
+    //                 case HandConfig.finger_conf_X.Straight:                                                                         break;
+    //                 case HandConfig.finger_conf_X.OneBent:                                                     xAngle[2] = ang;     break;
+    //                 case HandConfig.finger_conf_X.TwoSlitleyBent:  xAngle[0] = ang / 2;  xAngle[1] = ang / 2;                       break;
+    //                 case HandConfig.finger_conf_X.TwoBent:         xAngle[0] = ang;      xAngle[1] = ang;                           break;
+    //                 case HandConfig.finger_conf_X.AllSlitleyBent:  xAngle[0] = ang / 2;  xAngle[1] = ang / 2;  xAngle[2] = ang / 2; break;
+    //                 case HandConfig.finger_conf_X.AllBent:         xAngle[0] = ang;      xAngle[1] = ang;      xAngle[2] = ang;     break;
+
+    //                 default: throw new Exception("Invalid Configuration");
+    //             }
+    //         }
+    //         switch (conf_y[f])
+    //         {
+    //             case HandConfig.finger_conf_Y.Open:
+    //                 if (f == 0) // thumb
+    //                 {
+    //                     zAngle[2] = -ang/6; break;
+    //                 }
+    //                 else if (f == 1) // index
+    //                 {
+    //                     zAngle[2] = -ang/6; break;
+    //                 }
+    //                 else if (f == 3 | f == 4) // ring
+    //                 {
+    //                     zAngle[2] = ang/10; break;
+    //                 }
+    //                 else if (f == 3 | f == 4) // pinky
+    //                 {
+    //                     zAngle[2] = ang/6; break;
+    //                 }
+    //                 else // middle
+    //                 {
+    //                     break;
+    //                 }
+
+    //             case HandConfig.finger_conf_Y.Closed: break;
+
+    //             default: throw new Exception("Invalid Configuration");
+    //         }
+
+    //         Transform fj = fingers[f]; // fj is for Finger joint
+
+    //         for (int i = 0; i < 3; i++)
+    //         {
+    //             Vector3 angle = new Vector3(xAngle[i], 0f, zAngle[i]);
+    //             Vector3 updated_angle = new Vector3(angle.x - startRot[f*3 + i].x, 0f, angle.z - startRot[f*3 + i].z);
+
+    //             fj.Rotate(updated_angle, Space.Self);
+    //             fj = fj.parent;
+
+    //             // update current
+    //             startRot[f*3 + i].x += updated_angle.x;
+    //             startRot[f*3 + i].z += updated_angle.z;
+    //         }
+    //     }
+    // }
 }
