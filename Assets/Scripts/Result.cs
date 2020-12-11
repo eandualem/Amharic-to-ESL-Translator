315e67fa68aec23e1d587cdc7c34a76ebc5335d1
Nabeel Seid <nabeelseid@gmail.com>
2020-12-11 09:55:59 +0300

    Player and Result added

---
+++ Assets/Scripts/Result.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result
+{
+    public string originalText; // Amharic
+    public string translatedText;
+    public List<UnitAnimation> unit;
+
+    public Result ( string originalText, string translatedText, List<UnitAnimation> unit )
+    {
+        this.originalText = originalText;
+        this.translatedText = translatedText;
+        this.unit = unit;
+    }
+}
+
